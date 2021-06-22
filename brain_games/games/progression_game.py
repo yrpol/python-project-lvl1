99be0169d546a95b6a4ecde2241be7a1a11a6897
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-06-22 20:29:53 +0300

    fix progression game

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -8,20 +8,21 @@ def brain_progression():
         progression_length = 10
         step = random.randint(1, 7)
         first_number = random.randint(1, 15)
-        hiden_number_index = random.randint(0, progression_length)
+        hiden_number_index = random.randint(0, progression_length - 1)
 
         progression = ''
-        current_number = first_number
+        correct_answer = ''
+        current_number = str(first_number)
         index = 0
 
         while index < progression_length:
             if index == hiden_number_index:
                 progression += '.. '
-                correct_answer = str(current_number)
+                correct_answer = current_number
             else:
                 progression += '{} '.format(current_number)
 
-            current_number += step
+            current_number = str(int(current_number) + step)
             index += 1
 
         return (progression, correct_answer)
