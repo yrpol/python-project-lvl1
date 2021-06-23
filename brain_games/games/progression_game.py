a50f4f903128b5c11388d01f440e91aaf68e0086
Yurii Polishchuk <yr.polishchuk@gmail.com>
2021-06-23 14:36:43 +0300

    fix linter warnings

--- brain_games/games/progression_game.py
+++ brain_games/games/progression_game.py
@@ -3,28 +3,30 @@ import random
 
 condition = "What number is missing in the progression?"
 
+
 def play_round():
-        progression_length = 10
-        step = random.randint(1, 7)
-        first_number = random.randint(1, 15)
-        hiden_number_index = random.randint(0, progression_length - 1)
+    progression_length = 10
+    step = random.randint(1, 7)
+    first_number = random.randint(1, 15)
+    hiden_number_index = random.randint(0, progression_length - 1)
 
-        progression = ''
-        correct_answer = ''
-        current_number = str(first_number)
-        index = 0
+    progression = ''
+    correct_answer = ''
+    current_number = str(first_number)
+    index = 0
 
-        while index < progression_length:
-            if index == hiden_number_index:
-                progression += '.. '
-                correct_answer = current_number
-            else:
-                progression += '{} '.format(current_number)
+    while index < progression_length:
+        if index == hiden_number_index:
+            progression += '.. '
+            correct_answer = current_number
+        else:
+            progression += '{} '.format(current_number)
 
-            current_number = str(int(current_number) + step)
-            index += 1
+        current_number = str(int(current_number) + step)
+        index += 1
 
-        return (progression, correct_answer)
+    return (progression, correct_answer)
+
 
 def brain_progression():
     return condition, play_round
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.cli import welcome_user
 
+
 def main():
     welcome_user()
 
